786111c8bdb8216fd483b93cfcaab3c7ed55f326
samotny-vouk <147575490+samotny-vouk@users.noreply.github.com>
2024-06-02 11:39:32 +1000

    Add files via upload

--- new_some.py
+++ new_some.py
@@ -11,9 +11,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("noneuser_menu.ui", self)
-        self.login_but.clicked.connect(lambda: toNext(LoginWindow))
 
+        self.start_but.clicked.connect(lambda: toNext(StartWindow))
+        self.login_but.clicked.connect(lambda: toNext(LoginWindow))
         self.dictionary.clicked.connect(lambda: toNext(BaseDictionary))
+        self.flashcards.clicked.connect(lambda: toNext(CardsWindow))
+        self.settings.clicked.connect(lambda: toNext(SetWindow))
 
         self.vk_link.clicked.connect(lambda: webbrowser.open('#'))
         self.github_link.clicked.connect(
@@ -25,15 +28,36 @@ class MainLogged(QMainWindow):
     def __init__(self):
         super(MainLogged, self).__init__()
         loadUi("adv_user_menu.ui", self)
+        self.start_but.clicked.connect(lambda: toNext(StartWindow))
         self.login_but.clicked.connect(lambda: toNext(LoginWindow))
-        self.login_but.setText()
         self.dictionary_adv.clicked.connect(lambda: toNext(AdvDictionary))
+        self.flashcards.clicked.connect(lambda: toNext(CardsWindow))
+        self.settings.clicked.connect(lambda: toNext(SetWindow))
 
         self.vk_link.clicked.connect(lambda: webbrowser.open('#'))
-        self.github_link.clicked.connect(
-            lambda: webbrowser.open('https://github.com/samotny-vouk/PyQT-Project-2024-1courseFEFU-'))
+        self.github_link.clicked.connect(lambda: webbrowser.open('https://github.com/samotny-vouk/PyQT-Project-2024-1courseFEFU-'))
         self.telegram_link.clicked.connect(lambda: webbrowser.open('https://t.me/+QUVnH3rO-D80ZWYy'))
 
+class StartWindow(QMainWindow):
+    def __init__(self):
+        super(StartWindow, self).__init__()
+        loadUi("game_start.ui", self)
+
+        self.back.clicked.connect(lambda: toBack())
+
+class CardsWindow(QMainWindow):
+    def __init__(self):
+        super(CardsWindow, self).__init__()
+        loadUi("game_start.ui", self)
+
+        self.back.clicked.connect(lambda: toBack())
+
+class SetWindow(QMainWindow):
+    def __init__(self):
+        super(SetWindow, self).__init__()
+        loadUi("game_start.ui", self)
+
+        self.back.clicked.connect(lambda: toBack())
 
 class LoginWindow(QMainWindow):
     def __init__(self):
@@ -61,6 +85,16 @@ class RegisterWindow(QMainWindow):
     def __init__(self):
         super(RegisterWindow, self).__init__()
         loadUi("register_window.ui", self)
+
+        name = self.username_new.text()
+        mail = self.email_new.text()
+        password = self.password_new.text()
+        is_registered = db_handler.register(name, mail, password)
+        if is_registered:
+            self.register_new.clicked.connect(lambda: toNext(MainLogged))
+        else:
+            self.register_new.clicked.connect(lambda: toNext(RegisterWindow))
+
         self.back.clicked.connect(lambda: toBack())
 
 
@@ -68,7 +102,7 @@ class BaseDictionary(QMainWindow):
     def __init__(self):
         super(BaseDictionary, self).__init__()
         loadUi("dictionary_noneuser.ui", self)
-        #self.to_add_word.clicked.connect(self.goToAddingWords)
+        # self.to_add_word.clicked.connect(self.goToAddingWords)
         self.back.clicked.connect(lambda: toBack())
         # self.back1.clicked.connect(self.gotoMainWindow)
 
@@ -77,14 +111,9 @@ class AdvDictionary(QMainWindow):
     def __init__(self):
         super(AdvDictionary, self).__init__()
         loadUi("dictionary_advanced.ui", self)
-        self.to_add_word.clicked.connect(self.goToAddingWords)
+        self.to_add_word.clicked.connect(lambda: toNext(AddingWords))
         self.back.clicked.connect(lambda: toBack())
         # self.back1.clicked.connect(self.gotoMainWindow)
-
-    def goToAddingWords(self):
-        adding_words = AddingWords()
-        widget.addWidget(adding_words)
-        widget.setCurrentIndex(widget.currentIndex() + 1)
 
 
 class AddingWords(QMainWindow):
